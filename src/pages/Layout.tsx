088b203f0680c2397e2b1b824eeb428688f9c255
Erik Svanes <svanes.erik@gmail.com>
2025-04-29 17:13:50 -0400

    tailwind and better styling

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,15 +1,36 @@
-import { Outlet, Link } from "react-router-dom";
-import styles from "../styles/LayoutStyles.module.css";
+import { Outlet, NavLink } from "react-router-dom";
+
+const links = [
+  { link: "/", name: "Home" },
+  { link: "/experience", name: "Experience" },
+  { link: "/projects", name: "Projects" },
+];
 
 const Layout = () => {
   return (
-    <div className={styles.body}>
-      <nav className={styles.navigation}>
-        <Link to="/">Home</Link>
-        <Link to="/projects">Projects</Link>
-      </nav>
+    <div className="w-[100vw] h-[100vh] bg-black">
+      <div className="absolute w-[100%] flex justify-center mt-4 rounded-xl bg-black">
+        <nav className="flex gap-4 my-2 mx-4">
+          {links.map((link) => {
+            return (
+              <NavLink
+                className={({ isActive }) =>
+                  `text-center font-semibold py-2 px-4 rounded-lg hover:shadow-[0_0_3em_#646cff] transition-all border ${
+                    isActive
+                      ? "scale-105 border-[#646cff] bg-gradient-to-r from-blue-400 to-indigo-500 text-transparent bg-clip-text"
+                      : "border-transparent text-gray-200"
+                  }`
+                }
+                to={link.link}
+              >
+                {link.name}
+              </NavLink>
+            );
+          })}
+        </nav>
+      </div>
 
-      <div className={styles.children}>
+      <div className="w-[100%] h-[100%] pt-24 flex justify-center text-white">
         <Outlet />
       </div>
     </div>
